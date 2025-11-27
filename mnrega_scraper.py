a9e8b96655dafb3955206df8a3d7b9d9a6dc2384
Pavan Patel <patelpavan1205@gmail.com>
2025-11-28 04:03:26 +0530

    Railway chromium fix

--- mnrega_scraper.py
+++ mnrega_scraper.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-#from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
