11b46e15a9186b17902b82e2a3fd9ff745bfe94f
Pavan Patel <patelpavan1205@gmail.com>
2025-11-28 03:52:46 +0530

    Remove webdriver-manager and fix Chrome crash

--- mnrega_scraper.py
+++ mnrega_scraper.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+#from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
@@ -75,17 +75,19 @@ def run_mnrega_scraper(work_code: str, output_dir: str = ".") -> str:
 
     # Headless Chrome options
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless=new")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--log-level=3")
-
     options.binary_location = "/usr/bin/chromium"
+
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(
     service=Service("/usr/bin/chromedriver"),
     options=options
     )
+
     wait = WebDriverWait(driver, 40)
     driver.set_window_size(1400, 900)
 
